ffa244c20cbaf74d50e27a9416d33ea8e08e2836
Marcus Eggenberger <egs@quassel-irc.org>
2008-03-01 21:15:00 +0000

    fixed a bug where channels where always autojoined (even if explicitly parted)

--- src/core/SQL/SQLite/8/select_persistent_channels.sql
+++ src/core/SQL/SQLite/8/select_persistent_channels.sql
@@ -1,3 +1,3 @@
 SELECT buffername, key
 FROM buffer
-WHERE userid = :userid AND networkid = :networkid AND buffertype = 2
+WHERE userid = :userid AND networkid = :networkid AND buffertype = 2 AND joined = 1
