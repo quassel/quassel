2ef87aa6bfc05e76fb46cde4d9ed50975f70040c
Marcus Eggenberger <egs@quassel-irc.org>
2008-03-03 19:27:20 +0000

    further improved backlog replay speed

--- src/core/SQL/SQLite/9/select_messages.sql
+++ src/core/SQL/SQLite/9/select_messages.sql
@@ -1,7 +1,6 @@
 SELECT messageid, time,  type, flags, sender, message
 FROM backlog
-JOIN buffer ON backlog.bufferid = buffer.bufferid
 JOIN sender ON backlog.senderid = sender.senderid
-WHERE buffer.bufferid = :bufferid
+WHERE bufferid = :bufferid
 ORDER BY messageid DESC
 LIMIT :limit OFFSET :offset
--- src/core/SQL/SQLite/9/select_messagesSince.sql
+++ src/core/SQL/SQLite/9/select_messagesSince.sql
@@ -1,7 +1,6 @@
 SELECT messageid, time,  type, flags, sender, message
 FROM backlog
-JOIN buffer ON backlog.bufferid = buffer.bufferid
 JOIN sender ON backlog.senderid = sender.senderid
-WHERE buffer.bufferid = :bufferid AND backlog.time >= :since
+WHERE bufferid = :bufferid AND time >= :since
 ORDER BY messageid DESC
 LIMIT -1 OFFSET :offset
