d1b2699b8a77947f1b7dd67841c6ecb4ea1fce1e
Marcus Eggenberger <egs@quassel-irc.org>
2008-06-04 12:36:32 +0200

    some minor fixes:  - strange behavior of disabled autoadd for custom views  - permanently remvoed buffers disappear now instantly  - fixed a gcc warning BR #162

--- src/qtui/mainwin.cpp
+++ src/qtui/mainwin.cpp
@@ -520,7 +520,7 @@ void MainWin::closeEvent(QCloseEvent *event) {
 }
 
 void MainWin::systrayActivated( QSystemTrayIcon::ActivationReason activationReason) {
-  if (activationReason == QSystemTrayIcon::Trigger) {
+  if(activationReason == QSystemTrayIcon::Trigger) {
     toggleVisibility();
   }
 }
@@ -528,11 +528,12 @@ void MainWin::systrayActivated( QSystemTrayIcon::ActivationReason activationReas
 void MainWin::toggleVisibility() {
   if(isHidden() /*|| !isActiveWindow()*/) {
     show();
-    if(isMinimized())
-      if (isMaximized())
+    if(isMinimized()) {
+      if(isMaximized())
         showMaximized();
       else
         showNormal();
+    }
 
     raise();
     activateWindow();
