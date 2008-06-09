9e1bcc38bde6eb3eb1f867c17e0c1d9e1dc0b190
Marcus Eggenberger <egs@quassel-irc.org>
2008-06-09 22:54:38 +0200

    CAN I HAS CHANNEL MODES PLZ? Oh jeah Baby!

---
+++ src/core/corenetwork.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2005-08 by the Quassel Project                          *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "corenetwork.h"
+#include "coresession.h"
+
+CoreNetwork::CoreNetwork(const NetworkId &networkid, CoreSession *session)
+  : Network(networkid, session),
+    _coreSession(session)
+{
+}
+
+void CoreNetwork::requestConnect() const {
+  if(connectionState() != Disconnected) {
+    qWarning() << "Requesting connect while already being connected!";
+    return;
+  }
+  emit connectRequested(networkId());
+}
+
+void CoreNetwork::requestDisconnect() const {
+  if(connectionState() == Disconnected) {
+    qWarning() << "Requesting disconnect while not being connected!";
+    return;
+  }
+  emit disconnectRequested(networkId());
+}
---
+++ src/core/corenetwork.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2005-08 by the Quassel Project                          *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef CORENETWORK_H
+#define CORENETWORK_H
+
+#include "network.h"
+
+class CoreSession;
+
+class CoreNetwork : public Network {
+  Q_OBJECT
+
+public:
+  CoreNetwork(const NetworkId &networkid, CoreSession *session);
+
+  virtual const QMetaObject *syncMetaObject() const { return &Network::staticMetaObject; }
+
+  inline CoreSession *coreSession() const { return _coreSession; }
+
+public slots:
+  virtual void requestConnect() const;
+  virtual void requestDisconnect() const;
+
+private:
+  CoreSession *_coreSession;
+};
+
+#endif //CORENETWORK_H
