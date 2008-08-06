efa00e5a912c34c37bcc6dad7b8bc6e3a6f3e4fb
Manuel Nickschas <sputnick@quassel-irc.org>
2008-08-06 21:43:59 +0200

    Introduce AbstractMessageProcessor and its specialization QtUiMessageProcessor.
    Its task is to create ChatLineModelItems from Messages, which currently happens in Client.
    As this is too slow, we will once again process incoming messages, in particular backlog
    packages, asynchronously. Externalizing this and abstracting it away from Client will
    facilite this. Also the specialization in QtUi will allow us to use GUI features, which will
    be particularly handy for determining if we can do font rendering outside the GUI thread.

---
+++ src/client/abstractmessageprocessor.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+*   Copyright (C) 2005-08 by the Quassel Project                          *
+*   devel@quassel-irc.org                                                 *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) version 3.                                           *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include "abstractmessageprocessor.h"
+
+#include "client.h"
+
+AbstractMessageProcessor::AbstractMessageProcessor(QObject *parent) : QObject(parent) {
+
+
+
+}
+
+void AbstractMessageProcessor::process(Message &msg) {
+  processMessage(msg);
+  Client::buffer(msg.bufferInfo())->updateActivityLevel(msg);
+}
+
+void AbstractMessageProcessor::process(QList<Message> &msgs) {
+  processMessages(msgs);
+  foreach(Message msg, msgs) {
+    Client::buffer(msg.bufferInfo())->updateActivityLevel(msg);
+  }
+}
