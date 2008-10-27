7cc7b31044d82d24639956a8f004f0f6363d5e10
Marcus Eggenberger <egs@quassel-irc.org>
2008-10-27 16:12:47 +0100

    adding missing files... -.-

---
+++ src/client/backlogsettings.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2005-08 by the Quassel Project                          *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef BACKLOGSETTINGS_H
+#define BACKLOGSETTINGS_H
+
+#include "clientsettings.h"
+
+class BacklogSettings : public ClientSettings {
+public:
+  BacklogSettings() : ClientSettings("Backlog") {}
+  inline int requesterType() { return localValue("RequesterType", 0).toInt(); }
+  inline void setRequesterType(int requesterType) { setLocalValue("RequesterType", requesterType); }
+
+  inline int dynamicBacklogAmount() { return localValue("DynamicBacklogAmount", 200).toInt(); }
+  inline void setDynamicBacklogAmount(int amount) { return setLocalValue("DynamicBacklogAmount", amount); }
+
+  inline int fixedBacklogAmount() { return localValue("FixedBacklogAmount", 500).toInt(); }
+  inline void setFixedBacklogAmount(int amount) { return setLocalValue("FixedBacklogAmount", amount); }
+};
+
+#endif //BACKLOGSETTINGS_H
