be7b028fb2d78c462d15dd2fbd05e548a99cb4ef
Marcus Eggenberger <egs@quassel-irc.org>
2008-11-29 17:41:52 +0100

    missing files

---
+++ src/core/SQL/SQLite/11/select_messagesAll.sql
@@ -0,0 +1,8 @@
+SELECT messageid, bufferid, time,  type, flags, sender, message
+FROM backlog
+JOIN sender ON backlog.senderid = sender.senderid
+WHERE backlog.bufferid IN (SELECT bufferid FROM buffer WHERE userid = :userid)
+    AND backlog.messageid >= :firstmsg
+    AND backlog.messageid < :lastmsg
+ORDER BY messageid DESC
+LIMIT :limit
