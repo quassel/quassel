a2e7df3cffc6fa931288fcebb4145042c81b7c5d
Marcus Eggenberger <egs@quassel-irc.org>
2008-11-30 14:53:16 +0100

    Uncrapificating some queries... I must have been above ballmer peak when I wrote them... o_O

--- src/core/SQL/SQLite/11/select_messagesNew.sql
+++ src/core/SQL/SQLite/11/select_messagesNew.sql
@@ -1,8 +1,7 @@
 SELECT messageid, time,  type, flags, sender, message
 FROM backlog
-JOIN buffer ON backlog.bufferid = buffer.bufferid
 JOIN sender ON backlog.senderid = sender.senderid
-WHERE buffer.bufferid = :bufferid
+WHERE bufferid = :bufferid
     AND backlog.messageid >= :firstmsg
 ORDER BY messageid DESC
 LIMIT :limit
