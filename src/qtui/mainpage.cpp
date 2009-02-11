c8034446b66ab7964433235c51ce32bfc3c9d672
Manuel Nickschas <sputnick@quassel-irc.org>
2009-02-11 19:39:45 +0100

    Add new logo for the main page
    Probably only temporary until Nuno Pinheiro is kind enough to do more gorgeous artwork,
    but better than the old thing. Also we now have smooth scaling if the logo doesn't fit
    in its original size...

---
+++ src/qtui/mainpage.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+*   Copyright (C) 2005-09 by the Quassel Project                          *
+*   devel@quassel-irc.org                                                 *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) version 3.                                           *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include <QImage>
+#include <QPainter>
+
+#include "mainpage.h"
+
+MainPage::MainPage(QWidget *parent) : QWidget(parent) {
+
+
+}
+
+void MainPage::paintEvent(QPaintEvent *event) {
+  Q_UNUSED(event);
+
+  QPainter painter(this);
+  QImage img(":/pics/quassel-logo.png"); // FIXME load externally
+
+  if(img.height() > height() || img.width() > width())
+    img = img.scaled(width(), height(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+  qreal xmargin = (width() - img.width()) / 2;
+  qreal ymargin = (height() - img.height()) / 2;
+
+  painter.drawImage(xmargin, ymargin, img);
+
+}
---
+++ src/qtui/mainpage.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+*   Copyright (C) 2005-09 by the Quassel Project                          *
+*   devel@quassel-irc.org                                                 *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) version 3.                                           *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef MAINPAGE_H_
+#define MAINPAGE_H_
+
+#include <QWidget>
+
+class MainPage : public QWidget {
+  Q_OBJECT
+
+public:
+  MainPage(QWidget *parent = 0);
+
+protected:
+  void paintEvent(QPaintEvent *event);
+
+
+};
+
+#endif
