868e5d2bfef8b01e22fdc8b1256228a1f214e1bc
Manuel Nickschas <sputnick@quassel-irc.org>
2009-02-17 19:11:33 +0100

    Change wording in first-run wizard

--- src/qtui/ircconnectionwizard.cpp
+++ src/qtui/ircconnectionwizard.cpp
@@ -55,9 +55,8 @@ QWizardPage *IrcConnectionWizard::createIntroductionPage(QWidget *parent) {
   QWizardPage *page = new QWizardPage(parent);
   page->setTitle(QObject::tr("Welcome to Quassel IRC"));
 
-  QLabel *label = new QLabel(QObject::tr("This wizard will help you setting up your default identity and your IRC network connection. "
-					 "You can abort this wizard in any step, if you want to setup your identity and IRC connection in more detail. "
-					 "This can be done in the settings."), page);
+  QLabel *label = new QLabel(QObject::tr("This wizard will help you to set up your default identity and your IRC network connection.<br>"
+                                         "This only covers basic settings. You can cancel this wizard any time and use the settings dialog for more detailed changes."), page);
   label->setWordWrap(true);
 
   QVBoxLayout *layout = new QVBoxLayout;
