f03dadd142a691ab884f87c91c93c6c0a167e7e4
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-10 19:11:04 +0100

    updated copyright

--- src/qtui/snorenotificationbackend.h
+++ src/qtui/snorenotificationbackend.h
@@ -1,6 +1,6 @@
 /***************************************************************************
-*   Copyright (C) 2005-09 by the Quassel Project                          *
-*   devel@quassel-irc.org                                                 *
+*   Copyright (C) 2011 by the Patrick von Reth                            *
+*   patrick.vonreth@gmail.com                                             *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
