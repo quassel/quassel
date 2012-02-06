982bfbf8081be22ce7ce08adcf3ba92361ee7ca5
Daniel Albers <daniel@lbe.rs>
2012-02-06 17:23:25 +0100

    pull-from-transifex.sh: always attribute Last-Translator

--- po/pull-from-transifex.sh
+++ po/pull-from-transifex.sh
@@ -3,7 +3,16 @@
 tx pull $* &&
 git add po/*.po && (
   translators=$(while read mode pofile; do
-    translator=$(git diff --cached -- ${pofile} | perl -le 'while (<>) { if (/^\+(?:#|.*?:) *(.*?)(<[^@>]+@[^>]+>)/p) { $xltrs{$2} = $1 unless $xltrs{$2}; } last if /^\+"Last-Translator: /; }; push(@out, $n.$e) while (($e, $n) = each %xltrs); print(join(", ", @out));')
+    translator=$(git diff --cached -- ${pofile} | perl -le '
+      while (<>) {
+        if (/^(?:\+(?:#|.*?:)|[ +]"Last-Translator:) *(.*?)(<[^@>]+@[^>]+>)/p) {
+          $xltrs{$2} = $1 unless $xltrs{$2};
+          last if $& =~ /Last-Translator:/;
+        }
+      }
+      push(@out, $n.$e) while (($e, $n) = each %xltrs);
+      print(join(", ", @out));
+    ')
     lang=${pofile%.po}
     lang=${lang#po/}
     echo " - ${lang}: ${translator}"
