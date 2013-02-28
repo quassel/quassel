d0c0848307dab16108e855cab557be4a2f3d8d1b
Manuel Nickschas <sputnick@quassel-irc.org>
2013-02-28 20:34:52 +0100

    Relicense SystemTray and LegacySystemTray under LGPL 2+
    This was requested so that it could be made useful together with
    StatusNotifierItem (which came from KDE and already is LGPL'd).
    
    Here's hoping that it will result in a neat little library that programs
    can use to have a systemtray icon that sucks less than the stock one.
    
    This code was written by yours truly.

--- src/qtui/systemtray.h
+++ src/qtui/systemtray.h
@@ -2,10 +2,10 @@
  *   Copyright (C) 2005-2013 by the Quassel Project                        *
  *   devel@quassel-irc.org                                                 *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) version 3.                                           *
+ *   This file is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Library General Public License (LGPL)   *
+ *   as published by the Free Software Foundation; either version 2 of the *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
