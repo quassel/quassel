quassel/quassel

commit 4676ff82af669595edaf090c97a28161d67782a1

Don't display control characters
Replace them with their respective unicode char from the control picture
block.

src/uisupport/uistyle.cpp
@@ -561,15 +561,43 @@ UiStyle::StyledString UiStyle::styleString(const QString &s_, quint32 baseFormat
 
 QString UiStyle::mircToInternal(const QString &mirc_)
 {
-    QString mirc = mirc_;
-    mirc.replace('%', "%%");    // escape % just to be sure
-    mirc.replace('\t', "        ");    // tabs break layout, also this is italics in Konversation
-    mirc.replace('\x02', "%B");
-    mirc.replace('\x0f', "%O");
-    mirc.replace('\x12', "%R");
-    mirc.replace('\x16', "%R");
-    mirc.replace('\x1d', "%S");
-    mirc.replace('\x1f', "%U");
+    QString mirc;
+    mirc.reserve(mirc_.size());
+    foreach (const QChar &c, mirc_) {
+        if ((c < '\x20' || c == '\x7f') && c != '\x03') {
+            switch (c.unicode()) {
+                case '\x02':
+                    mirc += "%B";
+                    break;
+                case '\x0f':
+                    mirc += "%O";
+                    break;
+                case '\x12':
+                case '\x16':
+                    mirc += "%R";
+                    break;
+                case '\x1d':
+                    mirc += "%S";
+                    break;
+                case '\x1f':
+                    mirc += "%U";
+                    break;
+                case '\x7f':
+                    mirc += QChar(0x2421);
+                    break;
+                default:
+                    mirc += QChar(0x2400 + c.unicode());
+            }
+        } else {
+            if (c == '\t') {
+                mirc += "        ";
+                continue;
+            }
+            if (c == '%')
+                mirc += c;
+            mirc += c;
+        }
+    }
 
     // Now we bring the color codes (\x03) in a sane format that can be parsed more easily later.
     // %Dcfxx is foreground, %Dcbxx is background color, where xx is a 2 digit dec number denoting the color code.

commit 11d63f3740fd49eb32b5f604c17452913f9ae3d5

Fix replacing Horizontal Tab with the unicode char
Commit 4676ff82af669595edaf090c97a28161d67782a1 caused horizontal tabs
to be replaced with the HT character of the unicode control char block.
Fix that by replacing it with 8 spaces again.

src/uisupport/uistyle.cpp
@@ -572,6 +572,9 @@ QString UiStyle::mircToInternal(const QString &mirc_)
                 case '\x0f':
                     mirc += "%O";
                     break;
+                case '\x09':
+                    mirc += "        ";
+                    break;
                 case '\x12':
                 case '\x16':
                     mirc += "%R";
@@ -589,10 +592,6 @@ QString UiStyle::mircToInternal(const QString &mirc_)
                     mirc += QChar(0x2400 + c.unicode());
             }
         } else {
-            if (c == '\t') {
-                mirc += "        ";
-                continue;
-            }
             if (c == '%')
                 mirc += c;
             mirc += c;