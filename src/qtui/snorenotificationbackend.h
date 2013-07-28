b00cef666a48a9db42cc67c2cb6841e7efce7362
Patrick von Reth <vonreth@kde.org>
2013-07-28 13:34:50 +0200

    fixed typo

--- src/qtui/snorenotificationbackend.h
+++ src/qtui/snorenotificationbackend.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2011-2013 by the Patrick von Reth                       *
+*   Copyright (C) 2011-2013 by Patrick von Reth                           *
 *   vonreth@kde.org                                                       *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
