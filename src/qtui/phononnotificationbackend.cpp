ba6a9ce10750a65f0ba36281ceb45d7857ac076d
Manuel Nickschas <sputnick@quassel-irc.org>
2013-11-20 00:01:41 +0100

    Add QUrl include for Phonon
    Looks like some Qt versions just forward-declare QUrl, so this is
    needed.

--- src/qtui/phononnotificationbackend.cpp
+++ src/qtui/phononnotificationbackend.cpp
@@ -19,6 +19,7 @@
  ***************************************************************************/
 
 #include <QFileDialog>
+#include <QUrl>
 
 #include <phonon/mediaobject.h>
 #include <phonon/backendcapabilities.h>
