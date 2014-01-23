9c6f27c7bf7cb9dee234383ed9347c077aa2bce9
Manuel Nickschas <sputnick@quassel-irc.org>
2014-01-23 01:47:50 +0100

    Add PeerFactory
    This factory creates an instance of a RemotePeer subclass based on the
    given protocol type and peer-specific features. If given a list, it will
    select the preferred one (generally the first one that matches an available
    peer which accepts the given peer-specific features).
    
    For now, we just have the LegacyPeer (which does not have specific
    features, so that value is ignored).

---
+++ src/common/peerfactory.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2014 by the Quassel Project                        *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "peerfactory.h"
+
+#include "protocols/legacy/legacypeer.h"
+
+RemotePeer *PeerFactory::createPeer(const ProtoDescriptor &protocol, AuthHandler *authHandler, QTcpSocket *socket, QObject *parent)
+{
+    return createPeer(QVector<ProtoDescriptor>() << protocol, authHandler, socket, parent);
+}
+
+RemotePeer *PeerFactory::createPeer(const QVector<PeerFactory::ProtoDescriptor> &protocols, AuthHandler *authHandler, QTcpSocket *socket, QObject *parent)
+{
+    foreach(const ProtoDescriptor &protodesc, protocols) {
+        Protocol::Type proto = protodesc.first;
+        quint16 features = protodesc.second;
+        switch(proto) {
+            case Protocol::LegacyProtocol:
+                return new LegacyPeer(authHandler, socket, parent);
+            default:
+                break;
+        }
+    }
+
+    return 0;
+}
---
+++ src/common/peerfactory.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2014 by the Quassel Project                        *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef PEERFACTORY_H
+#define PEERFACTORY_H
+
+#include <QPair>
+
+#include "protocol.h"
+
+class QObject;
+class QTcpSocket;
+
+class AuthHandler;
+class RemotePeer;
+
+class PeerFactory
+{
+
+public:
+    // second value is the protocol-specific features
+    typedef QPair<Protocol::Type, quint16> ProtoDescriptor;
+
+    static RemotePeer *createPeer(const ProtoDescriptor &protocol, AuthHandler *authHandler, QTcpSocket *socket, QObject *parent = 0);
+    static RemotePeer *createPeer(const QVector<ProtoDescriptor> &protocols, AuthHandler *authHandler, QTcpSocket *socket, QObject *parent = 0);
+};
+
+#endif
