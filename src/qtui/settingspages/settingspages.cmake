0de0a17782cf478d638a532f36898197af88e0bc
Manuel Nickschas <sputnick@quassel-irc.org>
2014-03-17 23:15:57 +0100

    Enable automoc
    Finally we can make use of this very convenient CMake feature and have
    the moc stuff handled automagically. No need to list the headers anymore
    for feeding them into the long-deprecated qt4_wrap_cpp macro.
    
    Also, this should compile a bit faster and lead to slightly better optimized
    code, because automoc will combine all the generated moc stuff for a target
    into a single translation unit.
    
    Note that we could get rid of various empty .cpp files (where all the code
    is in the header) by just adding the header to the sources list; that way,
    automoc will pick them up. There is no need to have a dummy .cpp file for
    that.

--- src/qtui/settingspages/settingspages.cmake
+++ src/qtui/settingspages/settingspages.cmake
@@ -1,17 +1,50 @@
 # Putting $FOO in SETTINGSPAGES automatically includes
 # $FOOsettingspage.cpp, $FOOsettingspage.h and $FOOsettingspage.ui
 
-set(SETTINGSPAGES aliases appearance backlog bufferview chatview connection coreconnection chatmonitor coreaccount
-                  highlight identities ignorelist inputwidget itemview networks topicwidget)
+set(SETTINGSPAGES
+    aliases
+    appearance
+    backlog
+    bufferview
+    chatmonitor
+    chatview
+    connection
+    coreconnection
+    coreaccount
+    highlight
+    identities
+    ignorelist
+    inputwidget
+    itemview
+    networks
+    topicwidget
+)
 
 # Specify additional files (e.g. for subdialogs) here!
-set(SP_SOURCES aliasesmodel.cpp identityeditwidget.cpp ignorelistmodel.cpp notificationssettingspage.cpp previewbufferview.cpp)
-set(SP_HEADERS aliasesmodel.h identityeditwidget.h ignorelistmodel.h notificationssettingspage.h previewbufferview.h)
-set(SP_FORMS buffervieweditdlg.ui coreaccounteditdlg.ui createidentitydlg.ui identityeditwidget.ui ignorelisteditdlg.ui saveidentitiesdlg.ui 
-             networkadddlg.ui networkeditdlg.ui nickeditdlg.ui servereditdlg.ui)
+set(SP_SOURCES
+    aliasesmodel.cpp
+    identityeditwidget.cpp
+    ignorelistmodel.cpp
+    notificationssettingspage.cpp
+
+    # needed for automoc
+    previewbufferview.h
+)
+
+set(SP_FORMS
+    buffervieweditdlg.ui
+    coreaccounteditdlg.ui
+    createidentitydlg.ui
+    identityeditwidget.ui
+    ignorelisteditdlg.ui
+    networkadddlg.ui
+    networkeditdlg.ui
+    nickeditdlg.ui
+    saveidentitiesdlg.ui
+    servereditdlg.ui
+)
 
 if(NOT HAVE_KDE)
   set(SETTINGSPAGES ${SETTINGSPAGES} shortcuts)
   set(SP_SOURCES ${SP_SOURCES} keysequencewidget.cpp shortcutsmodel.cpp)
-  set(SP_HEADERS ${SP_HEADERS} keysequencewidget.h shortcutsmodel.h)
 endif(NOT HAVE_KDE)
