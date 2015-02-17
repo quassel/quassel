a2f27ecf68cf642313ddf37d0bf70cc25a33630d
Deas <neo_graal@hotmail.com>
2015-02-17 19:25:10 +0100

    Forgot to remove PostgresSQL 9.1 feature.

--- src/core/SQL/PostgreSQL/16/setup_010_sender.sql
+++ src/core/SQL/PostgreSQL/16/setup_010_sender.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS sender ( -- THE SENDER OF IRC MESSAGES
+CREATE TABLE sender ( -- THE SENDER OF IRC MESSAGES
        senderid serial NOT NULL PRIMARY KEY,
        sender varchar(255) UNIQUE NOT NULL
 )
