c69f844bae1f3fe7fe7d8e4c6fed750a32b55ee6
jjoyce <fuzzball81@gmail.com>
2015-05-07 15:03:02 -0400

    Python PEP8 cleanup of macosx_makebundle.py

--- scripts/build/macosx_makebundle.py
+++ scripts/build/macosx_makebundle.py
@@ -27,7 +27,7 @@ if len(sys.argv) < 2:
 SOURCE_DIR = sys.argv[1]
 
 if len(sys.argv) < 4:
-    BUNDLE_NAME= "Quassel Client"
+    BUNDLE_NAME = "Quassel Client"
     EXE_NAME = "quasselclient"
 else:
     EXE_NAME = sys.argv[3]
@@ -38,8 +38,9 @@ if(os.path.dirname(EXE_NAME)):
     CONTENTS_DIR = os.path.dirname(EXE_NAME) + "/"
 CONTENTS_DIR += BUNDLE_NAME + ".app/Contents/"
 
-BUNDLE_VERSION = commands.getoutput("git --git-dir="+SOURCE_DIR+"/.git/ describe")
+BUNDLE_VERSION = commands.getoutput("git --git-dir=" + SOURCE_DIR + "/.git/ describe")
 ICON_FILE = "pics/quassel.icns"
+
 
 def createBundle():
     try:
@@ -48,9 +49,11 @@ def createBundle():
     except:
         pass
 
+
 def copyFiles(exeFile, iconFile):
     os.system("cp %s %sMacOs/%s" % (exeFile, CONTENTS_DIR.replace(' ', '\ '), BUNDLE_NAME.replace(' ', '\ ')))
     os.system("cp %s/%s %s/Resources" % (SOURCE_DIR, iconFile, CONTENTS_DIR.replace(' ', '\ ')))
+
 
 def createPlist(bundleName, iconFile, bundleVersion):
     templateFile = file(SOURCE_DIR + "/scripts/build/Info.plist", 'r')
@@ -58,13 +61,12 @@ def createPlist(bundleName, iconFile, bundleVersion):
     templateFile.close()
 
     plistFile = file(CONTENTS_DIR + "Info.plist", 'w')
-    plistFile.write(template % {"BUNDLE_NAME" : bundleName,
-                                "ICON_FILE" : iconFile[iconFile.rfind("/")+1:],
-                                "BUNDLE_VERSION" : bundleVersion})
+    plistFile.write(template % {"BUNDLE_NAME": bundleName,
+                                "ICON_FILE": iconFile[iconFile.rfind("/") + 1:],
+                                "BUNDLE_VERSION": bundleVersion})
     plistFile.close()
 
 if __name__ == "__main__":
     createBundle()
     createPlist(BUNDLE_NAME, ICON_FILE, BUNDLE_VERSION)
     copyFiles(EXE_NAME, ICON_FILE)
-    pass
