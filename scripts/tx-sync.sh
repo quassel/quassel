5d3ba981e213f5d83ab57df9e06c87a9513f19d9
Daniel Albers <daniel@lbe.rs>
2016-02-11 01:17:08 +0100

    Add catalog update to tx-sync.sh.
    Transifex now offers automatic fetching of source files. Let tx-sync.sh
    regenerate and commit quassel.pot automatically to make use of that.

--- scripts/tx-sync.sh
+++ scripts/tx-sync.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -u
 quasselsrc=/usr/src/quassel
 localpobranch=i18n-tx-sync
+remote=origin
+branch=master
 
 pushd "$quasselsrc" && ( 
   currb=$(git name-rev --name-only HEAD)
   git checkout -q $localpobranch && (
-    git pull -q
-    EDITOR=/bin/true VISUAL=/usr/bin/editor "$quasselsrc"/po/pull-from-transifex.sh -f &&
-      git push -q
+    git pull -q --no-edit $remote $branch &&
+    pushd po/ &&
+    ./update-pot.sh &&
+    git commit -qm 'Update quassel.pot' ${quasselsrc}/po/quassel.pot
+    popd &&
+    "$quasselsrc"/po/pull-from-transifex.sh -f &&
+    git push -q
   ); git checkout -q "$currb"
 ); popd
