60ba221ec89296f4b66cbbab8b8d70673b92085a
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-03-01 22:52:08 +0100

    Remove default keybinding for showing/hiding menubar
    Resolves GH-191.

--- src/qtui/mainwin.cpp
+++ src/qtui/mainwin.cpp
@@ -406,7 +406,7 @@ void MainWin::setupActions()
     coll->addAction("ShowAwayLog", new Action(tr("Show Away Log"), coll,
             this, SLOT(showAwayLog())));
     coll->addAction("ToggleMenuBar", new Action(QIcon::fromTheme("show-menu"), tr("Show &Menubar"), coll,
-            0, 0, QKeySequence(Qt::CTRL + Qt::Key_M)))->setCheckable(true);
+            0, 0))->setCheckable(true);
 
     coll->addAction("ToggleStatusBar", new Action(tr("Show Status &Bar"), coll,
             0, 0))->setCheckable(true);
