c65cdada4836f01fe920b09c7fc861cb70c09ac7
Manuel Nickschas <sputnick@quassel-irc.org>
2016-03-01 20:01:14 +0100

    Fix inconsistent use of override in NickListWidget
    Clang warns about this.
    
    (cherry picked from commit 7ad7c792cae055a6549916ad190d6c8479bf674e)

--- src/qtui/nicklistwidget.h
+++ src/qtui/nicklistwidget.h
@@ -48,13 +48,13 @@ signals:
     void nickSelectionChanged(const QModelIndexList &);
 
 protected:
-    virtual QSize sizeHint() const;
-    virtual void hideEvent(QHideEvent *);
-    virtual void showEvent(QShowEvent *);
+    QSize sizeHint() const override;
+    void hideEvent(QHideEvent *) override;
+    void showEvent(QShowEvent *) override;
 
 protected slots:
-    virtual void currentChanged(const QModelIndex &current, const QModelIndex &previous);
-    virtual void rowsAboutToBeRemoved(const QModelIndex &parent, int start, int end);
+    void currentChanged(const QModelIndex &current, const QModelIndex &previous) override;
+    void rowsAboutToBeRemoved(const QModelIndex &parent, int start, int end) override;
 
 private slots:
     void removeBuffer(BufferId bufferId);
