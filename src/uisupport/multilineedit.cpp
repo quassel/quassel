2790d61fe2c52e0e8ef2f5f8fd7a4803e7492590
A. V. Lukyanov <dukzcry@users.noreply.github.com>
2016-08-11 10:57:27 +0300

    For OS X fix Bug #1212

--- src/uisupport/multilineedit.cpp
+++ src/uisupport/multilineedit.cpp
@@ -183,7 +183,11 @@ void MultiLineEdit::updateSizeHint()
     opt.lineWidth = lineWidth();
     opt.midLineWidth = midLineWidth();
     opt.state |= QStyle::State_Sunken;
-    QSize s = style()->sizeFromContents(QStyle::CT_LineEdit, &opt, QSize(100, h).expandedTo(QApplication::globalStrut()), this);
+    QWidget *widget = this;
+#ifdef Q_OS_MAC
+    widget = 0;
+#endif
+    QSize s = style()->sizeFromContents(QStyle::CT_LineEdit, &opt, QSize(100, h).expandedTo(QApplication::globalStrut()), widget);
     if (s != _sizeHint) {
         _sizeHint = s;
         updateGeometry();
