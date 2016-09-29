05148c3aea451609c6a75efbd4d350adbad46c2c
Manuel Nickschas <sputnick@quassel-irc.org>
2016-09-29 21:23:03 +0200

    dcc: Always clean up if a transfer completed or failed

--- src/common/transfer.cpp
+++ src/common/transfer.cpp
@@ -81,6 +81,9 @@ void Transfer::setStatus(Transfer::Status status)
         _status = status;
         SYNC(ARG(status));
         emit statusChanged(status);
+        if (status == Status::Completed || status == Status::Failed) {
+            cleanUp();
+        }
     }
 }
 
@@ -212,7 +215,6 @@ void Transfer::setError(const QString &errorString)
     qWarning() << Q_FUNC_INFO << errorString;
     emit error(errorString);
     setStatus(Status::Failed);
-    cleanUp();
 }
 
 
