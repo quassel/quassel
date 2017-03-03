df3e1433ea4850e57cade871ced1b0eab4fd785a
Alex Ingram <reimu@reimuhakurei.net>
2017-03-03 10:34:23 -0600

    Improve PostgreSQL performance for unread message fetch method, too.

--- src/core/SQL/PostgreSQL/20/select_messages.sql
+++ src/core/SQL/PostgreSQL/20/select_messages.sql
@@ -2,6 +2,6 @@ SELECT messageid, time,  type, flags, sender, message
 FROM backlog
 JOIN sender ON backlog.senderid = sender.senderid
 WHERE bufferid = $1
-AND messageid <= (SELECT buffer.lastmsgid FROM buffer WHERE buffer.bufferid = $1)
+AND backlog.messageid <= (SELECT buffer.lastmsgid FROM buffer WHERE buffer.bufferid = $1)
 ORDER BY messageid DESC
 LIMIT $2
