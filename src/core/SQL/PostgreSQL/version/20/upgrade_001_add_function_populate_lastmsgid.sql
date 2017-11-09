f509a0d1065d4e42c9eb84e0ffab9e72517df04c
Manuel Nickschas <sputnick@quassel-irc.org>
2017-11-09 23:23:22 +0100

    sql: Fix population of lastmsgid for empty buffers
    Buffers may be empty, in which case the query to determine the last
    message ID in them returns NULL, violating the constraint for the
    newly introduced column. Use 0 instead.
    
    Closes GH-315.

--- src/core/SQL/PostgreSQL/version/20/upgrade_001_add_function_populate_lastmsgid.sql
+++ src/core/SQL/PostgreSQL/version/20/upgrade_001_add_function_populate_lastmsgid.sql
@@ -5,12 +5,12 @@ BEGIN
 	FOR i IN SELECT * FROM buffer
 	LOOP
 		UPDATE buffer
-			SET lastmsgid = (
+			SET lastmsgid = COALESCE((
 				SELECT backlog.messageid
 				FROM backlog
 				WHERE backlog.bufferid = i.bufferid
 				ORDER BY messageid DESC LIMIT 1
-			)
+			), 0)
 			WHERE buffer.bufferid = i.bufferid;
 	END LOOP;
 	RETURN;
