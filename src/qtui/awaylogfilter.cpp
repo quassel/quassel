599d1ce88cadb73ae8b2e567ebadd841e01a9b13
Svetlana Tkachenko <svetlana@members.fsf.org>
2018-02-01 06:54:57 +1100

    exclude ignored messages (maybe Closes #1472)
    untested, maybe good if it compiles

--- src/qtui/awaylogfilter.cpp
+++ src/qtui/awaylogfilter.cpp
@@ -33,14 +33,24 @@ bool AwayLogFilter::filterAcceptsRow(int sourceRow, const QModelIndex &sourcePar
     QModelIndex source_index = sourceModel()->index(sourceRow, 0);
 
     Message::Flags flags = (Message::Flags)sourceModel()->data(source_index, MessageModel::FlagsRole).toInt();
+    
+    // Only show highlights from the backlog
     if (!(flags & Message::Backlog && flags & Message::Highlight))
         return false;
+    
+    // ignorelist handling
+    // only match if message is not flagged as server msg
+    if (!(flags & Message::ServerMsg) && Client::ignoreListManager()
+        && Client::ignoreListManager()->match(sourceIdx.data(MessageModel::MessageRole).value<Message>(), Client::networkModel()->networkName(bufferId)))
+return false;
 
+    // do not use invalid buffers
     BufferId bufferId = sourceModel()->data(source_index, MessageModel::BufferIdRole).value<BufferId>();
     if (!bufferId.isValid()) {
         return false;
     }
 
+    // only show messages that were sent after the user detached
     if (Client::networkModel()->lastSeenMsgId(bufferId) >= sourceModel()->data(source_index, MessageModel::MsgIdRole).value<MsgId>())
         return false;
 
