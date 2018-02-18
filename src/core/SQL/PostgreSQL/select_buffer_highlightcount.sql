cfd929ddfa437a51433b0ab051ad936cbcb6e82b
Janne Koschinski <janne@kuschku.de>
2018-02-18 21:14:57 +0100

    Ignore self messages

--- src/core/SQL/PostgreSQL/select_buffer_highlightcount.sql
+++ src/core/SQL/PostgreSQL/select_buffer_highlightcount.sql
@@ -3,5 +3,5 @@ FROM
   (SELECT SUM(1) AS sum FROM backlog
    WHERE bufferid = :bufferid
      AND flags & 2 != 0
-     AND flags & 1 == 0
+     AND flags & 1 = 0
      AND messageid > :lastseenmsgid) t;
