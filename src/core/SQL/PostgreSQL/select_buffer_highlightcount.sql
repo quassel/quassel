55998f4aa93e5d241611b6aa67533998d777d956
Janne Koschinski <janne@kuschku.de>
2018-02-18 21:41:54 +0100

    Use count(*) instead of sum(1)

--- src/core/SQL/PostgreSQL/select_buffer_highlightcount.sql
+++ src/core/SQL/PostgreSQL/select_buffer_highlightcount.sql
@@ -1,6 +1,6 @@
 SELECT COALESCE(t.sum,0)
 FROM
-  (SELECT SUM(1) AS sum FROM backlog
+  (SELECT COUNT(*) AS sum FROM backlog
    WHERE bufferid = :bufferid
      AND flags & 2 != 0
      AND flags & 1 = 0
