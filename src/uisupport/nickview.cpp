fe155e87ffa75bf4c13ae40311b029adba706816
Manuel Nickschas <sputnick@quassel-irc.org>
2018-02-27 10:59:52 +0100

    uisupport: Also fix doubleclick in nick views
    Since the Q_WS_* macros no longer exist in Qt5, replace them by the
    appropriate Q_OS_* macros. Remove some needless ones, too.

--- src/uisupport/nickview.cpp
+++ src/uisupport/nickview.cpp
@@ -53,11 +53,11 @@ NickView::NickView(QWidget *parent)
 
     connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), SLOT(showContextMenu(const QPoint &)));
 
-#if defined Q_WS_QWS || defined Q_WS_X11
-    connect(this, SIGNAL(doubleClicked(QModelIndex)), SLOT(startQuery(QModelIndex)));
-#else
+#if defined Q_OS_MACOS || defined Q_OS_WIN
     // afaik this is better on Mac and Windows
     connect(this, SIGNAL(activated(QModelIndex)), SLOT(startQuery(QModelIndex)));
+#else
+    connect(this, SIGNAL(doubleClicked(QModelIndex)), SLOT(startQuery(QModelIndex)));
 #endif
 }
 
