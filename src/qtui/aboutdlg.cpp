68878dc8366f2f4a0afe132847aad9a51a80cdbf
Manuel Nickschas <sputnick@quassel-irc.org>
2018-04-10 21:59:19 +0200

    Semi-yearly copyright bump
    It's no longer 2016.

--- src/qtui/aboutdlg.cpp
+++ src/qtui/aboutdlg.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005-2016 by the Quassel Project                        *
+ *   Copyright (C) 2005-2018 by the Quassel Project                        *
  *   devel@quassel-irc.org                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -61,7 +61,7 @@ QString AboutDlg::about() const
              "<a href=\"https://api.kde.org/frameworks/oxygen-icons5/html\">Oxygen icon theme</a> &copy; Nuno Pinheiro and others, licensed under the "
                  "<a href=\"https://github.com/KDE/oxygen-icons/blob/master/COPYING\">LGPLv3</a>.<br><br>"
              "Please use <a href=\"https://bugs.quassel-irc.org\">https://bugs.quassel-irc.org</a> to report bugs."
-        ).arg("2005-2016")
+        ).arg("2005-2018")
     };
 
     return res;
