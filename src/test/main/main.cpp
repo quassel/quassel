8f2ee00f4edef1693628d3af0bdee84d725eb754
Manuel Nickschas <sputnick@quassel-irc.org>
2018-09-24 20:36:21 +0200

    test: Add build system support and a main function for unit tests
    Add a new CMake option BUILD_TESTING (defaults to OFF) that, if
    enabled, will build unit tests and related requirements.
    
    Add a new library Quassel::Test::Main that provides a main function
    for test cases.
    
    Add a new CMake macro quassel_add_test for making the adding of test
    cases convenient by hiding most of the boilerplate.
    
    Test cases should #include "testglobal.h", so they automatically have
    access to GTest/GMock macros as well as the imported main function.

---
+++ src/test/main/main.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2018 by the Quassel Project                        *
+ *   devel@quassel-irc.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3.                                           *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "test-main-export.h"
+
+#include <gmock/gmock.h>
+
+#include <QCoreApplication>
+
+TEST_MAIN_EXPORT int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    QCoreApplication app(argc, argv);
+
+    return RUN_ALL_TESTS();
+}
