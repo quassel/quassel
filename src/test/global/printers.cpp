ea87685ab30b75dd6f9816d7a1136a605911e52d
Manuel Nickschas <sputnick@quassel-irc.org>
2018-10-04 18:59:59 +0200

    test: Add GTest printers for commonly used Qt types
    Because GTest by default prints out unknown types as hexdump or other
    badly readably formats, provide PrintTo functions for Qt types we use
    in tests. That way, GTest will use those instead of falling back to
    its own default.
    
    Reuse Qt's own QDebug stream support to avoid having to reimplement
    printing functionality for all the types. This makes it very easy
    to see e.g. the contents of a QVariantMap or other complex types.

--- src/test/global/printers.cpp
+++ src/test/global/printers.cpp
@@ -18,8 +18,50 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
-#pragma once
-
-#include <gmock/gmock.h>
-
 #include "printers.h"
+
+#include <QDebug>
+
+namespace {
+
+template<typename T>
+void debugOut(const T& value, ::std::ostream* os)
+{
+    // Just use Qt's own debug print support to print the value into a string
+    QString out;
+    QDebug dbg(&out);
+    dbg.nospace() << value;
+    *os << out.toStdString();
+}
+
+}  // anon
+
+void PrintTo(const QByteArray& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
+
+void PrintTo(const QDateTime& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
+
+void PrintTo(const QString& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
+
+void PrintTo(const QVariant& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
+
+void PrintTo(const QVariantList& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
+
+void PrintTo(const QVariantMap& value, std::ostream* os)
+{
+    debugOut(value, os);
+}
--- src/test/global/printers.h
+++ src/test/global/printers.h
@@ -20,6 +20,24 @@
 
 #pragma once
 
-#include <gmock/gmock.h>
+#include "test-global-export.h"
 
-#include "printers.h"
+#include <iostream>
+
+#include <QByteArray>
+#include <QDateTime>
+#include <QString>
+#include <QVariant>
+#include <QVariantList>
+#include <QVariantMap>
+
+#include "testglobal.h"
+
+// GTest printers for commonly used Qt types
+
+TEST_GLOBAL_EXPORT void PrintTo(const QByteArray&, std::ostream*);
+TEST_GLOBAL_EXPORT void PrintTo(const QDateTime&, std::ostream*);
+TEST_GLOBAL_EXPORT void PrintTo(const QString&, std::ostream*);
+TEST_GLOBAL_EXPORT void PrintTo(const QVariant&, std::ostream*);
+TEST_GLOBAL_EXPORT void PrintTo(const QVariantList&, std::ostream*);
+TEST_GLOBAL_EXPORT void PrintTo(const QVariantMap&, std::ostream*);
