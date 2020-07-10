acc3117ecc6a2709d560a6ebb63dac3b3dd5b4e3
Shane Synan <digitalcircuit36939@gmail.com>
2020-07-10 17:23:18 -0400

    core: Fix/unify SQL for forward message fetch
    NOTE: This commit should be squashed into the previous commit from
    justJanne.  It's committed separately just to make it easier to see
    what I changed.
    
    Fixup the SQLite and PostgreSQL queries to treat "<= 0" as any
    type/flag instead of just "= 0".  This matches the function
    definitions and fixes specifying "-1" instead of "0".  Without this
    change, specifying "-1" will result in missing messages!

--- src/core/SQL/PostgreSQL/select_messagesForward.sql
+++ src/core/SQL/PostgreSQL/select_messagesForward.sql
@@ -1,10 +1,10 @@
-SELECT messageid, time,  type, flags, sender, senderprefixes, realname, avatarurl, message
+SELECT messageid, time, type, flags, sender, senderprefixes, realname, avatarurl, message
 FROM backlog
 JOIN sender ON backlog.senderid = sender.senderid
 WHERE backlog.messageid >= $1
     AND backlog.messageid < $2
     AND bufferid = $3
-    AND backlog.type & $4 != 0
-    AND ($5 = 0 OR backlog.flags & $5 != 0)
+    AND ($4 <= 0 OR backlog.type & $4 != 0)
+    AND ($5 <= 0 OR backlog.flags & $5 != 0)
 ORDER BY messageid ASC
 LIMIT $6
