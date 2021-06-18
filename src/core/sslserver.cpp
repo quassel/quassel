1fc1282f0859daa4fa2cd42a7b4d3ab785765563
Chris <398094+phuzion@users.noreply.github.com>
2021-06-17 21:34:57 -0400

    Add link to certificate FAQ in --require-ssl error
    Include a link to the FAQ page explaining how to set up SSL support on the quassel-irc.org website.
    
    Co-authored-by: Shane Synan <digitalcircuit36939@gmail.com>

--- src/core/sslserver.cpp
+++ src/core/sslserver.cpp
@@ -53,7 +53,8 @@ SslServer::SslServer(QObject* parent)
         // do not proceed, throw an exception and quit. This prevents the core from falling
         // back to a plaintext-only core when they should be expecting SSL/TLS only.
         if (Quassel::isOptionSet("require-ssl")) {
-            throw ExitException{EXIT_FAILURE, tr("--require-ssl is set, but no SSL certificate is available. Exiting.")};
+            throw ExitException{EXIT_FAILURE, tr("--require-ssl is set, but no SSL certificate is available. Exiting.\n"
+                                                 "Please see https://quassel-irc.org/faq/cert to learn how to enable SSL support.")};
         }
         if (!sslWarningShown) {
             qWarning() << "SslServer: Unable to set certificate file\n"
