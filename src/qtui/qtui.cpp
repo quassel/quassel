f61bc43cf7aee0640d4d5d89dedcaf008db29647
Manuel Nickschas <sputnick@quassel-irc.org>
2022-01-01 15:50:27 +0100

    src: Yearly copyright bump
    This time on time!

--- src/qtui/qtui.cpp
+++ src/qtui/qtui.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005-2020 by the Quassel Project                        *
+ *   Copyright (C) 2005-2022 by the Quassel Project                        *
  *   devel@quassel-irc.org                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
