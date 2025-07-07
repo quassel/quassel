a145873c6ab985930dc785ab34f0571b6fe21b6b
Aaron Bauman <bauman.aaron@gmail.com>
2025-07-07 14:38:50 -0400

    Fix overloads and update util.h header

--- src/common/util.h
+++ src/common/util.h
@@ -22,8 +22,6 @@
 
 #include "common-export.h"
 
-#include <optional>
-
 #include <QList>
 #include <QSet>
 #include <QString>
@@ -47,10 +45,10 @@ COMMON_EXPORT QString secondsToString(int timeInSeconds);
 /** This function takes a string and first checks if it is encoded in utf8, in which case it is
  *  decoded appropriately. Otherwise, the specified text codec is used to transform the string.
  *  \param input The input string containing encoded data
- *  \param codec The text codec we use if the input is not utf8
+ *  \param encoding The encoding we use if the input is not utf8
  *  \return The decoded string.
  */
-COMMON_EXPORT QString decodeString(const QByteArray& input, const std::optional<QStringConverter>& codec = std::nullopt);
+COMMON_EXPORT QString decodeString(const QByteArray& input, QStringConverter::Encoding encoding = QStringConverter::Utf8);
 
 COMMON_EXPORT uint editingDistance(const QString& s1, const QString& s2);
 
@@ -142,4 +140,4 @@ struct SelectOverloadHelper
  * @tparam Args Argument types of the desired signature
  */
 template<typename... Args>
-constexpr Q_DECL_UNUSED detail::SelectOverloadHelper<Args...> selectOverload = {};
+constexpr detail::SelectOverloadHelper<Args...> selectOverload = {};
